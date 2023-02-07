856c92e61be3282c83a567348861431cf74690e5
Ričardas Brazdžius <ricardas.brazdzius@tietoevry.com>
2023-02-07 15:31:07 +0200

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(110, 50%, 70%)",
+        secondary: "hsl(141, 43%, 62%)",
+        tertiary: "hsl(163, 41%, 52%)",
+      },
+    },
   },
   plugins: [],
 };
