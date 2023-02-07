9f80b50b6b6a2884cbc520f788915c45fa320b86
Ričardas Brazdžius <ricardas.brazdzius@tietoevry.com>
2023-02-07 18:34:10 +0200

    update default to just export

--- src/ui/container/container.component.tsx
+++ src/ui/container/container.component.tsx
@@ -4,6 +4,6 @@ type Props = {
   children: ReactNode;
 };
 
-export default function Container({ children }: Props): JSX.Element {
+export function Container({ children }: Props): JSX.Element {
   return <div className="container mx-auto">{children}</div>;
 }
